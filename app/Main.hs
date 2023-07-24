0490045f85a835c67da1877b745cdaaa58ed5337
Penelope Yong <penelopeysm@gmail.com>
2023-07-24 02:50:44 +0100

    Initial commit

---
+++ app/Main.hs
@@ -0,0 +1,21 @@
+module Main where
+
+import Control.Monad ((>=>))
+import Data.Text (Text)
+import Pokeapi
+
+getHA :: Text -> IO (Maybe Text)
+getHA p = do
+  pkmn <- pokemon p
+  case filter paIsHidden (pokemonAbilities pkmn) of
+    [] -> pure Nothing
+    (x : _) -> do
+      abty <- ability $ name (paAbility x)
+      let englishName = filter (\n -> name (nameLanguage n) == "en") (abilityNames abty)
+      pure $ case englishName of
+        [] -> Nothing
+        (n : _) -> Just $ nameName n
+
+main :: IO ()
+main = do
+  mapM_ (getHA >=> print) ["Azumarill", "Piplup", "Shaymin-Land", "Togekiss", "Togepi", "Whimsicott"]
